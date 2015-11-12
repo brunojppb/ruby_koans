e22b67ae6faeb31f9d688961a7d724588194da8d
Bruno Paulino <brunojppb@gmail.com>
2015-11-12 20:54:37 -0300

    About strings

--- triangle.rb
+++ triangle.rb
@@ -16,11 +16,10 @@
 def triangle(a, b, c)
   raise TriangleError, "values iqual to 0" if a == 0 || b == 0 || c == 0
   raise TriangleError, "Negative Values" if a < 0 || b < 0 || c < 0
-  raise TriangleError, "Wrong values" if [a,b,c].each { |value| return false if }
+  # raise TriangleError, "Wrong values" if [a,b,c].each { |value| return false if }
   return :equilateral if ((a == b) && (b == c))
   return :isosceles if ((a == b) || (a == c) || (b == c))
   return :scalene if (a != b && b != c)
-  raise Tri
 end
 
 # Error class used in part 2.  No need to change this code.
